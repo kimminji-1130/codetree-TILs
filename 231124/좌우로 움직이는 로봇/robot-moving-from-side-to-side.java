8ce73ad1fd4f71509c1a2dbcc5ca7c0e5163415d
김민지 <84611998+kimminji-1130@users.noreply.github.com>
2023-11-24 17:12:56 +0900

    Title: 좌우로 움직이는 로봇, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231124/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
+++ 231124/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -1,74 +1,82 @@
 import java.util.Scanner;
+import java.util.Arrays;
+
+// 악수의 정보를 나타내는 클래스 선언
+class Shake implements Comparable<Shake> {
+    int time;
+    int person1;
+    int person2;
+    
+    public Shake(int time, int person1, int person2) {
+        this.time = time;
+        this.person1 = person1;
+        this.person2 = person2;
+    }
+
+    @Override
+    public int compareTo(Shake shake) {
+        // 시간을 기준으로 오름차순으로 정렬합니다.
+        return time - shake.time;
+    }
+};
 
 public class Main {
-    public static final int MAX_T = 1000000;
+    public static final int MAX_T = 250;
+    public static final int MAX_N = 100;
     
-    public static int n, m;
-    public static int[] posA = new int[MAX_T  + 1];
-    public static int[] posB = new int[MAX_T  + 1];
+    public static int n, k, p, t;
+    public static int[] shakeNum = new int[MAX_N + 1];
+    public static boolean[] infected = new boolean[MAX_N + 1];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // 입력
         n = sc.nextInt();
-        m = sc.nextInt();
+        k = sc.nextInt();
+        p = sc.nextInt();
+        t = sc.nextInt();
+        infected[p] = true;
         
-        // A가 매 초마다 서있는 위치를 기록
-        int timeA = 1;
-        for(int i = 0; i < n; i++) {
-            int t = sc.nextInt();
-            char d = sc.next().charAt(0);
+        Shake[] shakes = new Shake[MAX_T];
+        
+        for(int i = 0; i < t; i++) {
+            int time = sc.nextInt();
+            int person1 = sc.nextInt();
+            int person2 = sc.nextInt();
+            // Shake 객체를 생성해 넣어줍니다.
+            shakes[i] = new Shake(time, person1, person2);
+        }
+        
+        // custom comparator를 활용한 정렬
+        Arrays.sort(shakes, 0, t);
+        
+        // 각 악수 횟수를 세서,
+        // K번 초과로 악수를 했을 시 전염시키지 않습니다.
+        for(int i = 0; i < t; i++) {
+            int target1 = shakes[i].person1;
+            int target2 = shakes[i].person2;
             
-            while(t-- > 0) {
-                if(d == 'R')
-                    posA[timeA] = posA[timeA - 1] + 1;
-                else
-                    posA[timeA] = posA[timeA - 1] - 1;
-                
-                timeA++;
-            }
-        }
-        
-        // B가 매 초마다 서있는 위치를 기록
-        int timeB = 1;
-        for(int i = 0; i < m; i++) {
-            int t = sc.nextInt();
-            char d = sc.next().charAt(0);
+            // 감염되어 있을 경우 악수 횟수를 증가시킵니다.
+            if(infected[target1])
+                shakeNum[target1]++;
+            if(infected[target2])
+                shakeNum[target2]++;
             
-            while(t-- > 0) {
-                if(d == 'R')
-                    posB[timeB] = posB[timeB - 1] + 1;
-                else
-                    posB[timeB] = posB[timeB - 1] - 1;
-                
-                timeB++;
-            }
+            // target1이 감염되어 있고 아직 K번 이하로 악수했다면 target2를 전염시킵니다.
+            if(shakeNum[target1] <= k && infected[target1])
+                infected[target2] = true;
+            
+            // target2가 감염되어 있고 아직 K번 이하로 악수했다면 target1을 전염시킵니다.
+            if(shakeNum[target2] <= k && infected[target2])
+                infected[target1] = true;
         }
         
-        if(timeA < timeB) {
-            for(int i = timeA; i < timeB; i++) {
-                posA[i] = posA[i - 1];
-            }
-        }
-        else if(timeA > timeB) {
-            for(int i = timeB; i < timeA; i++) {
-                posB[i] = posB[i - 1];
-            }
+        for(int i = 1; i <= n; i++) {
+            if(infected[i])
+                System.out.print(1);
+            else
+                System.out.print(0);
         }
         
-        // 새롭게 만나는 횟수를 구합니다.
-        int cnt = 0;
-        int timeMax = 0;
-        if(timeA < timeB)
-            timeMax = timeB;
-        else
-            timeMax = timeA;
-        
-        for(int i = 1; i < timeMax; i++) {
-            if(posA[i] == posB[i] && posA[i - 1] != posB[i - 1])
-                cnt++;
-        }
-        
-        System.out.print(cnt);
     }
 }
