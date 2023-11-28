3a82974b91927e65991089abdd2b469483538c90
김민지 <84611998+kimminji-1130@users.noreply.github.com>
2023-11-28 17:02:08 +0900

    Title: 이상한 폭탄 2, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 231128/이상한 폭탄 2/strange-bomb-2.java
+++ 231128/이상한 폭탄 2/strange-bomb-2.java
@@ -1,54 +1,35 @@
 import java.util.Scanner;
 
 public class Main {
-    public static final int MAX_K = 10;
-    public static final int MAX_N = 20;
+    public static final int MAX_N = 100;
     
-    public static int k, n;
-    public static int[][] arr = new int[MAX_K][MAX_N];
+    public static int n, k;
+    public static int[] num = new int[MAX_N];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // 입력
-        k = sc.nextInt();
         n = sc.nextInt();
+        k = sc.nextInt();
         
-        for(int i = 0; i < k; i++)
-            for(int j = 0; j < n; j++)
-                arr[i][j] = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            num[i] = sc.nextInt();
+        
+        int ans = -1;
 
-        int ans = 0;
-
-        // 모든 쌍에 대해서 불변의 순위인 쌍을 찾습니다.
-        for(int i = 1; i <= n; i++)
-            for(int j = 1; j <= n; j++) {
-                // i번 개발자가 j번 개발자보다 항상 높은 순위인지 여부를 확인합니다.
-
-                // i와 j가 같을 경우 넘어갑니다.
-                if(i == j)
+        // 모든 쌍에 대해서 터질 수 있는 폭탄을 찾고
+        // 그 중 번호가 최대인 값을 찾습니다.
+        for(int i = 0; i < n; i++)
+            for(int j = i + 1; j < n; j++) {
+                // 거리가 k를 초과하는 경우 넘어갑니다.
+                if(j - i > k)
+                    break;
+                
+                // 두 폭탄의 번호가 다를 경우 터지지 않습니다.
+                if(num[i] != num[j])
                     continue;
                 
-                // correct : i번 개발자가 j번 개발자보다 항상 높은 순위일때 true
-                boolean correct = true;
-
-                // k번의 모든 경기에 대해서 두 개발자의 위치를 찾고,
-                // 하나라도 i번 개발자가 더 뒤에 있으면 correct를 false로 바꿉니다.
-                for(int x = 0; x < k; x++) {
-                    int indexI = 0, indexJ = 0;
-
-                    for(int y = 0; y < n; y++) {
-                        if(arr[x][y] == i)
-                            indexI = y;
-                        if(arr[x][y] == j)
-                            indexJ = y;
-                    }
-
-                    if(indexI > indexJ)
-                        correct = false;
-                }
-
-                if(correct)
-                    ans++;
+                ans = Math.max(ans, num[i]);
             }
         
         System.out.print(ans);
