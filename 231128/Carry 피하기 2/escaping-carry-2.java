ef2eaa1bcec359f09eb48703a8433f2cfd7540e0
김민지 <84611998+kimminji-1130@users.noreply.github.com>
2023-11-28 16:55:57 +0900

    Title: Carry 피하기 2, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 231128/Carry 피하기 2/escaping-carry-2.java
+++ 231128/Carry 피하기 2/escaping-carry-2.java
@@ -1,24 +1,45 @@
 import java.util.Scanner;
 
 public class Main {
+    public static final int MAX_N = 20;
+    
     public static int n;
-    public static String str;
+    public static int[] arr = new int[MAX_N];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // 입력
         n = sc.nextInt();
-        str = sc.next();
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
         
         // 모든 쌍을 다 잡아봅니다.
-        int cnt = 0;
+        int ans = -1;
         for(int i = 0; i < n; i++)
             for(int j = i + 1; j < n; j++)
-                for(int k = j + 1; k < n; k++)
-                    if(str.charAt(i) == 'C' && str.charAt(j) == 'O'
-                        && str.charAt(k) == 'W')
-                        cnt++;
+                for(int k = j + 1; k < n; k++) {
+                    boolean carry = false;
+                    
+                    //일의 자리에서 carry가 발생하는 경우
+                    if(arr[i] % 10 + arr[j] % 10 + arr[k] % 10 >= 10)
+                        carry = true;
+                    
+                    //십의 자리에서 carry가 발생하는 경우
+                    if(arr[i] % 100 / 10 + arr[j] % 100 / 10 + arr[k] % 100 / 10 >= 10)
+                        carry = true;
+                    
+                    //백의 자리에서 carry가 발생하는 경우
+                    if(arr[i] % 1000 / 100 + arr[j] % 1000 / 100 + arr[k] % 1000 / 100 >= 10)
+                        carry = true;
+                    
+                    //천의 자리에서 carry가 발생하는 경우
+                    if(arr[i] % 10000 / 1000 + arr[j] % 10000 / 1000 + arr[k] % 10000 / 1000 >= 10)
+                        carry = true;
+                    
+                    if(carry == false)
+                        ans = Math.max(ans, arr[i] + arr[j] + arr[k]);
+                }
         
-        System.out.print(cnt);
+        System.out.print(ans);
     }
 }
